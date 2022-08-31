e77c4374ccdf5d44097742e7a1187288dfab4e5a
vphoton <keengman.2018@mymail.becker.edu>
2022-08-30 18:13:10 -0600

    Update SetPlayerDescriptions.lua

--- SetPlayerDescriptions.lua
+++ SetPlayerDescriptions.lua
@@ -1,28 +1,26 @@
 local Players = game:GetService("Players")
 
+local function RemoveMeshes(Character)
+	local Humanoid = Character:WaitForChild("Humanoid")
+	wait()
+	local CurrentDescription = Humanoid:GetAppliedDescription() 
 
-function PlayerJoined(Player)
-	local function RemoveMeshes(Character)
-		local Humanoid = Character:WaitForChild("Humanoid")
-		wait()
-		local CurrentDescription = Humanoid:GetAppliedDescription() 
+	CurrentDescription.Head = 0
+	CurrentDescription.Torso = 0
+	CurrentDescription.LeftArm = 0
+	CurrentDescription.RightArm  = 0
+	CurrentDescription.LeftLeg = 0
+	CurrentDescription.RightLeg = 0
 
-		CurrentDescription.Head = 0
-		CurrentDescription.Torso = 0
-		CurrentDescription.LeftArm = 0
-		CurrentDescription.RightArm  = 0
-		CurrentDescription.LeftLeg = 0
-		CurrentDescription.RightLeg = 0
-		
-		CurrentDescription.BodyTypeScale = 0.3
-		CurrentDescription.DepthScale = 1
-		CurrentDescription.HeadScale = 1
-		CurrentDescription.HeightScale = 1
-		CurrentDescription.ProportionScale = 1
-		CurrentDescription.WidthScale = 1
-		Humanoid:ApplyDescription(CurrentDescription) 
-
-	end
+	CurrentDescription.BodyTypeScale = 0.3
+	CurrentDescription.DepthScale = 1
+	CurrentDescription.HeadScale = 1
+	CurrentDescription.HeightScale = 1
+	CurrentDescription.ProportionScale = 1
+	CurrentDescription.WidthScale = 1
+	Humanoid:ApplyDescription(CurrentDescription) 
+end
+local function PlayerJoined(Player)
 	Player.CharacterAdded:Connect(RemoveMeshes)
 end
 
