335240cade5ce3a64cb0435eacca1978739262d5
vphoton <keengman.2018@mymail.becker.edu>
2022-09-29 22:27:41 -0600

    Create tweeninfoBase.lua

---
+++ tweeninfoBase.lua
@@ -0,0 +1,8 @@
+local Info = TweenInfo.new(
+	4, --The Tween will take 4 seconds.
+	Enum.EasingStyle.Sine, --The tween will use the "Sine" EasingStyle.
+	Enum.EasingDirection.In, --The tween will use the "In" EasingDirection.
+	5, --Repeat count (numbers < 0 will repeat indefinitely)
+	true, --The tween reverses.
+	1 --The tween will delay for 1 second before repeating.
+)
