41a109fe73d53a8dde90bf88d1d7e49f74a919db
vphoton <keengman.2018@mymail.becker.edu>
2022-12-19 21:11:06 -0700

    Create ArmsVisible.lua

---
+++ ArmsVisible.lua
@@ -0,0 +1,29 @@
+-- should work with both R15 and R6
+-- make this a localscript in StarterCharacterScripts
+
+local char = script.Parent
+local humanoid = char:WaitForChild("Humanoid")
+
+local visibleParts = {
+	"Left Arm",
+	"Right Arm",
+	"LeftUpperArm",
+	"LeftLowerArm",
+	"LeftHand",
+	"RightUpperArm",
+	"RightLowerArm",
+	"RightHand",
+}
+
+function antiTrans(part)
+	if part and part:IsA("BasePart") and table.find(visibleParts,part.Name) then
+		part.LocalTransparencyModifier = part.Transparency
+		part.Changed:connect(function (property)    
+			part.LocalTransparencyModifier = part.Transparency
+		end)
+	end
+end
+
+for _,v in pairs(char:GetChildren()) do
+	antiTrans(v)
+end
