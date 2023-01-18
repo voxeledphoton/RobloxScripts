85f635a8cd7b6305b43b70f2b39e19a0666d2d9c
vphoton <keengman.2018@mymail.becker.edu>
2023-01-18 14:56:46 -0700

    Update BasicDatastore.lua

--- BasicDatastore.lua
+++ BasicDatastore.lua
@@ -6,52 +6,61 @@ local RS = game:GetService("ReplicatedStorage")
 local Players = game:GetService("Players")
 
 local function onPlayerAdded(plr)
-	local dataLoaded = PData:GetAsync(plr.UserId)
-	if not dataLoaded then
-		local data = {}
-		-- first time data entry
-		data.Name = plr.Name
-		data.AccountAge = plr.AccountAge
-		data.FirstTimeJoined = os.time()
-		data.TimesJoined = 0
-		data.LastTimeJoined = os.time()
-		data.Dabloons = 0
-		data.Version = 1 -- CHANGE THIS EVERYTIME YOU UPDATE THE DATA
-		-- new in version 2
-
-		dataLoaded = data
-		PData:SetAsync(plr.UserId, http:JSONEncode(data))
-	else
-		-- data entry update
-		dataLoaded = http:JSONDecode(dataLoaded)
-		
-		--[[
-		if dataLoaded.Version == 1 then
-			-- add keys that version 1 didn't have and update version
-			dataLoaded.Version = 2
-		end
-		]]
-
-		-- update some keys values
-		dataLoaded.LastTimeJoined = os.time()
-		dataLoaded.MembershipType = plr.MembershipType
-		dataLoaded.AccountAge = plr.AccountAge
-		dataLoaded.TimesJoined = dataLoaded.TimesJoined + 1
-
-		PData:SetAsync(plr.UserId, http:JSONEncode(dataLoaded))
-	end
-
-	PData:UpdateAsync("PLAYER_INDEX", function(oldData)
-		local data = oldData or {}
-		data[plr.UserId] = true --so as to avoid duplicate entries
-		return data
+	local dataLoaded = nil
+	local success, updatedData = pcall(function()
+		dataLoaded = PData:GetAsync(plr.UserId)
 	end)
+	if success then
+		if not dataLoaded then
+			local data = {}
+			-- first time data entry
+			data.Name = plr.Name
+			data.AccountAge = plr.AccountAge
+			data.FirstTimeJoined = os.time()
+			data.TimesJoined = 0
+			data.LastTimeJoined = os.time()
+			data.Dabloons = 0
+			data.Version = 1 -- CHANGE THIS EVERYTIME YOU UPDATE THE DATA
+			-- new in version 2
 
-	-- add dabloons instance value to player
-	local dabloons = Instance.new("IntValue")
-	dabloons.Value = dataLoaded.Dabloons
-	dabloons.Name = "Dabloons"
-	dabloons.Parent = plr
+			dataLoaded = data
+			PData:SetAsync(plr.UserId, http:JSONEncode(data))
+		else
+			-- data entry update
+			local success, updatedData = pcall(function()
+				dataLoaded = http:JSONDecode(dataLoaded)
+			end)
+			--[[
+			if dataLoaded.Version == 1 then
+				-- add keys that version 1 didn't have and update version
+				dataLoaded.Version = 2
+			end
+			]]
+
+			-- update some keys values
+			dataLoaded.LastTimeJoined = os.time()
+			dataLoaded.MembershipType = plr.MembershipType
+			dataLoaded.AccountAge = plr.AccountAge
+			dataLoaded.TimesJoined = dataLoaded.TimesJoined + 1
+			local success, updatedData = pcall(function()
+				PData:SetAsync(plr.UserId, http:JSONEncode(dataLoaded))
+			end)
+		end
+		-- since keys hold 4mb this might fill after 400k players since each userId is approx 10 characters
+		local success, updatedData = pcall(function()
+			PData:UpdateAsync("PLAYER_INDEX", function(oldData)
+				local data = oldData or {}
+				data[plr.UserId] = true --so as to avoid duplicate entries
+				return data
+			end)
+		end)
+
+		-- add dabloons instance value to player
+		local dabloons = Instance.new("IntValue")
+		dabloons.Value = dataLoaded.Dabloons
+		dabloons.Name = "Dabloons"
+		dabloons.Parent = plr
+	end
 end
 
 Players.PlayerAdded:Connect(onPlayerAdded)
@@ -61,10 +70,15 @@ end
 
 local function save(plr)
 	-- save unlocked blocks
-	local dataLoaded = PData:GetAsync(plr.UserId)
-	dataLoaded = http:JSONDecode(dataLoaded)
-	dataLoaded.Dabloons = plr:WaitForChild("Dabloons").Value
-	PData:SetAsync(plr.UserId, http:JSONEncode(dataLoaded))
+	local dataLoaded = nil
+	local success, updatedData = pcall(function()
+		dataLoaded = PData:GetAsync(plr.UserId)
+		dataLoaded = http:JSONDecode(dataLoaded)
+	end)
+	if success then
+		dataLoaded.Dabloons = plr:WaitForChild("Dabloons").Value
+		PData:SetAsync(plr.UserId, http:JSONEncode(dataLoaded))
+	end
 end
 
 Players.PlayerRemoving:Connect(function(plr)
